751439641e40688a7469b4ea67c937bda65e6ed1
user149138 <user149138@eu504.local>
2019-03-05 00:45:16 +0000

    Initial Commit

---
+++ Cwk1/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Cwk1
+//
+//  Created by user149138 on 3/5/19.
+//  Copyright © 2019 user149138. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
